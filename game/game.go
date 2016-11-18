2b38d8dcb1cf163254ba5009ae017c7f034e57f5
Andrews Medina <andrewsmedina@gmail.com>
2016-11-18 00:54:43 -0200

    using opengl to draw the window

--- game/game.go
+++ game/game.go
@@ -1,5 +1,9 @@
 package game
 
+import (
+	"github.com/go-gl/glfw/v3.2/glfw"
+)
+
 // Options represents a game options
 type Options struct {
 	Size    []int
@@ -10,6 +14,14 @@ type game struct{}
 
 // New returns a new game
 func New(opts *Options) *game {
+	glfw.Init()
+	defer glfw.Terminate()
+	window, _ := glfw.CreateWindow(800, 600, "Cube", nil, nil)
+	window.MakeContextCurrent()
+	for !window.ShouldClose() {
+		window.SwapBuffers()
+		glfw.PollEvents()
+	}
 	return &game{}
 }
 
