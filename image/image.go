63288194bd41486981c2126fa55975c46bee30ae
Andrews Medina <andrewsmedina@gmail.com>
2016-11-21 21:12:36 -0300

    add image load

--- image/image.go
+++ image/image.go
@@ -19,5 +19,9 @@ func (i *img) Rect() *rect {
 
 // Load load an image
 func Load(src string) *img {
+	file, _ := os.Open(src)
+	i, _, _ := image.Decode(file)
+	rgba := image.NewRGBA(i.Bounds())
+	draw.Draw(rgba, rgba.Bounds(), i, image.Point{0, 0}, draw.Src)
 	return &img{}
 }
