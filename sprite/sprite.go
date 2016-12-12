c37f2f29d4fc70d580f85a6cb8d45e3112e5e430
Andrews Medina <andrewsmedina@gmail.com>
2016-12-12 01:23:26 -0200

    add comments

--- sprite/sprite.go
+++ sprite/sprite.go
@@ -4,13 +4,16 @@ import (
 	"golang.org/x/mobile/gl"
 )
 
+// Sprite represents a sprite
 type Sprite struct {
 	image    string
 	position []int
 }
 
+// New creates a new sprite
 func New(image string, position []int) *Sprite {
 	return &Sprite{image: image, position: position}
 }
 
+// Draw draws the sprite in the gl context
 func (s *Sprite) Draw(ctx gl.Context) {}
