0f05e68a67a44eb8e04c85e53469f06de277b059
Andrews Medina <andrewsmedina@gmail.com>
2016-12-13 22:43:36 -0200

    add comments in layer

--- layer/layer.go
+++ layer/layer.go
@@ -8,18 +8,22 @@ type drawer interface {
 	Draw(gl.Context)
 }
 
+// Layer represents a layer
 type Layer struct {
 	d drawer
 }
 
+// New returns a new Layer
 func New() *Layer {
 	return &Layer{}
 }
 
+// Add adds a drawer component to layer
 func (l *Layer) Add(d drawer) {
 	l.d = d
 }
 
+ // Draw draws layer elements in the given context
 func (l *Layer) Draw(ctx gl.Context) {
 	l.d.Draw(ctx)
 }
