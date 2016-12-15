50ab91deeb89bcf9ccfe293014ef79bfeb0db354
Andrews Medina <andrewsmedina@gmail.com>
2016-12-15 18:34:03 -0200

    director: send time and size event on render

--- director/director.go
+++ director/director.go
@@ -2,13 +2,16 @@ package director
 
 import (
 	"fmt"
+	"time"
 
 	"github.com/andrewsmedina/aquarela/scene"
 	"golang.org/x/mobile/app"
 	"golang.org/x/mobile/event/lifecycle"
 	"golang.org/x/mobile/event/paint"
+	"golang.org/x/mobile/event/size"
 	"golang.org/x/mobile/exp/gl/glutil"
 	"golang.org/x/mobile/exp/sprite"
+	"golang.org/x/mobile/exp/sprite/clock"
 	"golang.org/x/mobile/exp/sprite/glsprite"
 	"golang.org/x/mobile/gl"
 )
@@ -17,10 +20,12 @@ type director struct {
 	scene  *scene.Scene
 	images *glutil.Images
 	eng    sprite.Engine
+	n *sprite.Node
+	startTime time.Time
 }
 
 func New() *director {
-	return &director{}
+	return &director{startTime: time.Now()}
 }
 
 func (d *director) Run(s *scene.Scene) {
@@ -38,16 +43,18 @@ func (d *director) off() {
 	d.images.Release()
 }
 
-func (d *director) draw(ctx gl.Context) {
+func (d *director) draw(ctx gl.Context, sz size.Event) {
 	ctx.ClearColor(0, 0, 0, 1)
 	ctx.Clear(gl.COLOR_BUFFER_BIT)
 	d.scene.Draw(ctx)
-	d.eng.Render(scene, now, sz)
+	now := clock.Time(time.Since(d.startTime) * 60 / time.Second)
+	d.eng.Render(d.n, now, sz)
 }
 
 func (d *director) loop() {
 	app.Main(func(a app.App) {
 		var glctx gl.Context
+		var sz size.Event
 		for e := range a.Events() {
 			switch e := a.Filter(e).(type) {
 			case lifecycle.Event:
@@ -62,9 +69,11 @@ func (d *director) loop() {
 					glctx = nil
 					fmt.Println("off")
 				}
+			case size.Event:
+				sz = e
 			case paint.Event:
 				fmt.Println("paint")
-				d.draw(glctx)
+				d.draw(glctx, sz)
 				a.Publish()
 				a.Send(paint.Event{})
 			}
